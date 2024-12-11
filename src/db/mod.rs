ce54c3c5ca2e9865a702a0c615ec18e7ba616015
Artem Markov <drownbes@gmail.com>
2024-12-11 17:37:14 +0200

    Try using embeddings with sqlite

---
+++ src/db/mod.rs
@@ -0,0 +1,96 @@
+use ollama_rs::generation::embeddings::request::GenerateEmbeddingsRequest;
+use ollama_rs::Ollama;
+use sqlx::ConnectOptions;
+use sqlx::sqlite::{SqliteConnectOptions, SqliteJournalMode};
+use std::str::FromStr;
+use sqlite_vec::sqlite3_vec_init;
+use libsqlite3_sys::sqlite3_auto_extension;
+use zerocopy::{IntoBytes};
+use sqlx::Row;
+use sqlx::Connection;
+
+
+async fn run() -> anyhow::Result<()> {
+
+    unsafe {
+        sqlite3_auto_extension(Some(std::mem::transmute(sqlite3_vec_init as *const ())));
+    }
+    
+    let mut conn = SqliteConnectOptions::from_str("sqlite::memory:")?
+    .journal_mode(SqliteJournalMode::Wal)
+    .connect().await?;
+
+    sqlx::query("create table embeds(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        text TEXT,
+        embedding FLOAT[1024]
+    )").execute(&mut conn).await?;
+
+
+
+    let ollama = Ollama::default();
+
+    let apple = "striploin steak";
+    let orange = "entrecote steak";
+    let car = "beef burger";
+
+    let words = vec![apple, orange, car, "horse dick stew"];
+    let mut ns = vec![];
+
+    for word in words {
+        let request = GenerateEmbeddingsRequest::new("snowflake-arctic-embed2".to_string(), word.into());
+        let res = ollama.generate_embeddings(request).await.unwrap();
+        ns.push((word, res.embeddings[0].clone()));
+    }
+
+
+    let mut tx = conn.begin().await?;
+    //
+    //
+    for (t, e) in ns.iter() {
+        let v : &[u8] = e.as_bytes();
+        sqlx::query("insert into embeds(text, embedding) values(?, ?)")
+        .bind(t)
+        .bind(v)
+        .execute(&mut *tx).await?;
+    }
+    tx.commit().await?;
+
+    let v : &[u8] = ns[0].1.as_bytes();
+
+    let rr = sqlx::query("
+        select text, 
+            vec_distance_L2(embedding, ?) as distance,
+            vec_distance_L1(embedding, ?),
+            vec_distance_cosine(embedding, ?)
+        from embeds")
+        .bind(v)
+        .bind(v)
+        .bind(v)
+        .fetch_all(&mut conn).await?;
+
+
+    for r in rr {
+        let ver : String = r.get(0);
+        let l2 : f32 = r.get(1);
+        let l1 : f32 = r.get(1);
+        let cosine : f32 = r.get(1);
+        println!("{} {} {} {}", ver, l2, l1, cosine);
+    }
+
+    Ok(())
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_db() {
+        let r = run().await;
+        dbg!(r);
+    }
+}
+
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 mod barbora;
 mod rimi;
 mod selver;
+mod db;
