1640f3ca354c9b324fc0ddb6ee3acf66fd057602
Artem Markov <drownbes@gmail.com>
2025-01-04 13:07:39 +0200

    add method to generate with olama

--- src/ollama.rs
+++ src/ollama.rs
@@ -1,4 +1,6 @@
 use anyhow::anyhow;
+use ollama_rs::generation::chat::request::ChatMessageRequest;
+use ollama_rs::generation::chat::ChatMessage;
 use ollama_rs::generation::embeddings::request::GenerateEmbeddingsRequest;
 use ollama_rs::Ollama;
 
@@ -26,10 +28,18 @@ impl OllamaRunner {
             .nth(0)
             .ok_or(anyhow!("Empty embeddings returned"))
     }
+
+    pub async fn generate(&self, prompt: &str, message: &str) -> anyhow::Result<Option<String>> {
+        let msg = format!("{} \n\n {}", prompt, message);
+        let request = ChatMessageRequest::new("llama3.1:8b".to_string(), 
+            vec![ChatMessage::user(msg.to_string())]
+        );
+        let res = self.ollama.send_chat_messages(request).await?;
+        Ok(res.message.map(|x| x.content))
+    }
 }
 
-
-/*
+const PROMPT: &str = "
 You are an expert butcher with extensive knowledge on different types of meat. You are especially good at determining beef cut by estonian name. You were born in Estonia and lived there for 20 years after that you become a proficient butcher at the US. You lovely estonian mother will die if you make a mistake on determining beef cut. I will give you estonian online shop beef product name and you will give me beef cut name of that product in english.  
 
 For example:
@@ -47,4 +57,23 @@ Your answer:
 Skirt Steak, 95 
 
 Remember that is important. Your mother would be fucked by the ugly ape if you add comments. You should always answer in format: Beef cut, confidence level. And remember don't comment anything, just answer.
-*/
+";
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_beef_cut_detector() {
+        let ollama = OllamaRunner::new(&OllamaConfig {
+            host: "127.0.0.1".into(),
+            port: 11434,
+            embedding_model: "snowflake-arctic-embed2".into()
+
+
+        });
+        let answer = ollama.generate(PROMPT, "Veise antrekoot, RAKVERE LK, kg").await.unwrap();
+        dbg!(answer);
+        
+    }
+}
